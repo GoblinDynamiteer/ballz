60c94789df8f110ca12c076ba4002b570f8c12b7
Johan Kämpe <kampe.johan@gmail.com>
2017-10-10 20:48:04 +0200

    General cleanup

--- def.h
+++ def.h
@@ -5,7 +5,7 @@
  *      Author: kampe_000
  */
 
-/*	 Include guard	*/
+/* Include guard    */
 #ifndef DEF_H_
 #define DEF_H_
 
@@ -20,98 +20,98 @@
 #include "SDL_image.h"
 #include "SDL_ttf.h"
 
-/*	 Window dimensions, FULL HD!	*/
+/* Window dimensions, FULL HD!    */
 #define WIN_WIDTH 1920
 #define WIN_HEIGHT 1080
 
-/*	 Flags for window	*/
-#define WIN_FLAGS 0//SDL_WINDOW_FULLSCREEN
+/* Flags for window    */
+#define WIN_FLAGS SDL_WINDOW_FULLSCREEN
 #define REN_FLAGS SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
 
-/*	 Max balls on screen	*/
+/* Max balls on screen    */
 #define MAX_BALLS 5000
 
-/*	 Balls per "tick" of spray (right button)	*/
+/* Balls per "tick" of spray (right button)    */
 #define BALLS_PER_SPRAY 3
 
-/*	 Ball art size. Height == Width	*/
+/* Ball art size. Height == Width    */
 #define BALL_SIZE 50
 
-/*	 Ball movement speed & acceleration	*/
+/* Ball movement speed & acceleration    */
 #define MAXSPEED 15.0
 #define ACC 1.005
 
-/*	 Radius for magnet and repel	*/
+/* Radius for magnet and repel    */
 #define MAG_RADIUS 500
 #define MAGSTR 6.5
 #define REPEL_RADIUS 350
 
 #define KILLSIZE 45
 
-/*	 Font stuff	*/
+/* Font stuff    */
 #define FONT_SIZE 28
 
-/*	 Cursor stuff	*/
-/*	 Blade mode rotation	*/
+/* Cursor stuff    */
+/* Blade mode rotation    */
 #define BLADE_SPEED 10.5
 
-/*	 Game modes // Cursors	*/
+/* Game modes // Cursors    */
 enum {NORMAL, MAGNET, REPEL, BLADE, NEO, MAX_GAME_TYPES};
 
-/*	 Ball colors, for texture array index	*/
+/* Ball colors, for texture array index    */
 enum {RED, YELLOW, BLUE, GREEN, SMILEY, PURPLE, DEVIL, MAX_BALL_ART};
 
 enum {VERTICAL, HORIZONTAL};
 
-/*	 Struct definitions	*/
+/* Struct definitions    */
 typedef struct velocity{
-	float x, y;
+    float x, y;
 }velocity;
 
 typedef struct pos{
-	int x, y;
+    int x, y;
 }position;
 
 typedef struct ball{
-	short color;
-	bool active;
-	SDL_Rect rect;
-	velocity speed;
-	double angle;
+    short color;
+    bool active;
+    SDL_Rect rect;
+    velocity speed;
+    double angle;
 }ball;
 
 typedef struct ballsGame{
-	SDL_Window * window;
-	SDL_Renderer * renderer;
-	ball * balls[MAX_BALLS];
-	SDL_Texture * cursorArt[MAX_GAME_TYPES];
-	SDL_Texture * shield;
-	position cursor;
-	double cursorAngle;
-	short mode;
-	TTF_Font *font;
-	int createdBalls;
-	int killedBalls;
-	int grabbedBallIndex;
-	SDL_Texture * ballArt[MAX_BALL_ART];
-	int shieldFrame;
-	Uint64 ticker;
-	char * modeText[MAX_GAME_TYPES];
+    SDL_Window * window;
+    SDL_Renderer * renderer;
+    ball * balls[MAX_BALLS];
+    SDL_Texture * cursorArt[MAX_GAME_TYPES];
+    SDL_Texture * shield;
+    position cursor;
+    double cursorAngle;
+    short mode;
+    TTF_Font *font;
+    int createdBalls;
+    int killedBalls;
+    int grabbedBallIndex;
+    SDL_Texture * ballArt[MAX_BALL_ART];
+    int shieldFrame;
+    Uint64 ticker;
+    char * modeText[MAX_GAME_TYPES];
 }ballsGame;
 
 
-/*	 Function definitions	*/
+/* Function definitions    */
 
-/*	 load.c	*/
+/* load.c    */
 bool loadGame(ballsGame * game);
 
-/*	 event.c	*/
+/* event.c    */
 bool processEvent(ballsGame * game);
 float getRandomDirection(void);
-void cycleGameMode(ballsGame * game, bool upDown);
+void cycleGameMode(ballsGame * game, bool cycle_direction);
 double calculateDistance(int x1, int x2, int y1, int y2);
 
-/*	 balls.c	*/
+/* balls.c    */
 bool addBall(ballsGame * game, int x, int y);
 void moveBall(ballsGame * game, int i);
 void rotateBall(ballsGame * game, int i);
@@ -125,7 +125,7 @@ void flipBallSpeed(ballsGame * game, int i, bool direction);
 void freezeBalls(ballsGame * game, int i);
 int grabBall(ballsGame * game);
 
-/*	 draw.c	*/
+/* draw.c    */
 bool renderGame(ballsGame * game);
 void renderCursor(ballsGame * game);
 void renderBalls(ballsGame * game);
--- main.c
+++ main.c
@@ -7,56 +7,57 @@
 
 #include "def.h"
 
-int main(int argc, char *argv[]){
-	/*	 Init SDL and SDL subsystems	*/
+int main(int argc, char *argv[])
+{
+    /*     Init SDL and SDL subsystems    */
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
     TTF_Init();
 
-    /*	 Hide cursor	*/
+    /*     Hide cursor    */
     SDL_ShowCursor(SDL_DISABLE);
 
-    /*	 Declare window and renderer	*/
+    /*     Declare window and renderer    */
     SDL_Window * window;
     SDL_Renderer * renderer;
 
-    /*	 Create window	*/
-	window = SDL_CreateWindow(
-		"Ballz!",
-		SDL_WINDOWPOS_UNDEFINED,
-		SDL_WINDOWPOS_UNDEFINED,
-		WIN_WIDTH,
-		WIN_HEIGHT,
-		WIN_FLAGS
-	);
+    /*     Create window    */
+    window = SDL_CreateWindow(
+        "Ballz!",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        WIN_WIDTH,
+        WIN_HEIGHT,
+        0x00 // Use WIN_FLAGS for full screen
+    );
 
-    /*	 Create renderer	*/
-	renderer = SDL_CreateRenderer(
-			window,
-			-1,
-			REN_FLAGS
-	);
+    /*     Create renderer    */
+    renderer = SDL_CreateRenderer(
+            window,
+            -1,
+            REN_FLAGS
+    );
 
 
-    /*	 Declare struct ballsGame	*/
+    /*     Declare struct ballsGame    */
     ballsGame ballsGame;
     ballsGame.window = window;
     ballsGame.renderer = renderer;
 
-    /*	 Load game	*/
+    /*     Load game    */
     loadGame(&ballsGame);
 
     while(processEvent(&ballsGame)){
-    	renderGame(&ballsGame);
-    	SDL_Delay(1000/60);
+        renderGame(&ballsGame);
+        SDL_Delay(1000/60);
     }
 
 
-	for(int i = 0; i < MAX_BALLS; i++){
-		free(ballsGame.balls[i]);
-	}
+    for(int i = 0; i < MAX_BALLS; i++){
+        free(ballsGame.balls[i]);
+    }
 
-	/*	 Quit SDL	*/
+    /*     Quit SDL    */
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
     TTF_CloseFont(ballsGame.font);
@@ -64,20 +65,20 @@ int main(int argc, char *argv[]){
     window = NULL;
     ballsGame.font = NULL;
 
-	for(int i = 0; i < MAX_BALL_ART; i++){
-		SDL_DestroyTexture(ballsGame.ballArt[i]);
-		ballsGame.ballArt[i] = NULL;
-	}
+    for(int i = 0; i < MAX_BALL_ART; i++){
+        SDL_DestroyTexture(ballsGame.ballArt[i]);
+        ballsGame.ballArt[i] = NULL;
+    }
 
-	for(int i = 0; i< MAX_GAME_TYPES; i++){
-		SDL_DestroyTexture(ballsGame.cursorArt[i]);
-		ballsGame.cursorArt[i] = NULL;
-	}
+    for(int i = 0; i< MAX_GAME_TYPES; i++){
+        SDL_DestroyTexture(ballsGame.cursorArt[i]);
+        ballsGame.cursorArt[i] = NULL;
+    }
 
     //Quit SDL subsystems
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
 
-	return 0;
+    return 0;
 }
